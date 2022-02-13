29959cbc7e5e156d6d264158e2175ec0a4c6418f
laura <laura@secretchaingirl.io>
2022-02-13 16:24:15 -0500

    fix deploy/instantiate scripts

--- contracts/scripts/query.sh
+++ contracts/scripts/query.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#CONTRACT='secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg'
 CONTRACT=''
 QUERY_MSG='{"get_motd":{}}'
 
--- contracts/scripts/update.sh
+++ contracts/scripts/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-UPDATE_MSG='{"update":{"motd":"Hello_Crypto!"}}'
+#CONTRACT='secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg'
 CONTRACT=''
+UPDATE_MSG='{"update":{"motd":"#thesecretisout"}}'
+
 secretd tx compute execute $CONTRACT $UPDATE_MSG --from a -y --keyring-backend test | jq
